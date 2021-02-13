a2df2ef9024aeefe30f82f2b3eb1be9dee7f3fff
Billy Lewis <billy@Billys-MacBook-Pro.local>
2021-02-13 00:37:44 -0500

    fixed css bug

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,174 @@
+//Styling Map
+const styleMap = [
+  {
+      "featureType": "all",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "weight": "2.00"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#9c9c9c"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text",
+      "stylers": [
+          {
+              "visibility": "on"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [
+          {
+              "color": "#f2f2f2"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [
+          {
+              "saturation": -100
+          },
+          {
+              "lightness": 45
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#eeeeee"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#7b7b7b"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "simplified"
+          }
+      ]
+  },
+  {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [
+          {
+              "color": "#46bcec"
+          },
+          {
+              "visibility": "on"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#c8d7d4"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#070707"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+  }
+]
 // [GSU MAP]
 // Initialize and add the map and map3 inside div
 let map;
@@ -13,13 +184,14 @@ let markers = [];
 function initMap() {
   // The coordinates of GSU
   const coords = {
-    lat: 33.754435,
-    lng: -84.388888,
+    lat: 33.754449701207854,
+    lng: -84.385359680176,
   };
   // create map and center it on GSU
   map = new google.maps.Map(document.getElementById('mapImage'), {
-    zoom: 17,
+    zoom: 16,
     center: coords,
+    styles: styleMap
   });
   // create map and center it on Atlanta
   map3 = new google.maps.Map(document.getElementById('directionImage'), {
@@ -28,6 +200,7 @@ function initMap() {
       lng: -84.388,
     },
     zoom: 13,
+    styles: styleMap
   });
 
   infoWindow = new google.maps.InfoWindow();
